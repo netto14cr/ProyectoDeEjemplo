9114ab4d3582ce5b3ea1104ed308385f08afb7c0
NESTOR LEIVA MORA <nestor.leiva.mora@est.una.ac.cr>
2020-09-01 02:22:11 -0600

    Mi primer código sincronizado con phpstorm

---
+++ admin/config/global.php
@@ -0,0 +1,24 @@
+<?php 
+//ip de la pc servidor base de datos
+
+//define("DB_HOST", "sql207.epizy.com");
+define("DB_HOST", "localhost");
+// nombre de la base de datos
+define("DB_NAME", "control_asistencia");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "root");
+//define("DB_USERNAME", "epiz_25518321");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "");
+//define("DB_PASSWORD", "lQwHfzlfqLl");
+
+//codificación de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("epiz_25518321_XXX", "Control gestor asistencia");
+ 
+ ?>
