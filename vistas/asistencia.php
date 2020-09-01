a04286f4342a1bc860167f06c8b69701c50524a9
David 14 cr <netto__14@hotmail.com>
2020-09-01 17:51:05 -0600

    Cambios finales de david 14 cr

--- vistas/asistencia.php
+++ vistas/asistencia.php
@@ -1,9 +1,9 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ejemplo de cambio netto</title>
+    <title>Ejemplo de Ejemplo</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
@@ -59,7 +59,6 @@
   <!-- /.lockscreen-item -->
   <div class="help-block text-center">
     Ingresa tu nombre como ID</div>
-    <p>cambios en rama netto14cr</p>
   <div class="text-center">
 
   </div>
